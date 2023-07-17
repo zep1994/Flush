2135d13f831312c1e35b60ea5260033c42a4d43b
zep1994 <thomasmatlockbba@gmail.com>
2023-07-17 14:00:40 -0500

    Add Get by ID

--- Flush_API/Controllers/IbsCountController.cs
+++ Flush_API/Controllers/IbsCountController.cs
@@ -2,6 +2,7 @@
 using Flush_API.Data;
 using Flush_API.Dtos;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 
 namespace Flush_API.Controllers
 {
@@ -19,12 +20,23 @@ namespace Flush_API.Controllers
             _mapper = mapper;
         }
 
-        [HttpGet]
+        [HttpGet("{id}")]
         public async Task<ActionResult<IEnumerable<IbsCountReadDto>>> GetAllIbsCounts()
         {
             var count = await _repo.GetAllIbsCounts();
             Console.WriteLine($"{count}");
             return Ok(_mapper.Map<IEnumerable<IbsCountReadDto>>(count));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<IbsCountReadDto>>> GetIbsCountById(int id)
+        {
+            var ibsCountModel = await _repo.GetIbsCountById(id);
+            if (ibsCountModel != null)
+            {
+                return Ok(_mapper.Map<IbsCountReadDto>(ibsCountModel));
+            }
+            return NotFound();
         }
     }
 }
