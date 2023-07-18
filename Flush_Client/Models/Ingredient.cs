c53c63d3f771e54b318dcd1f2732cbc6c06969d8
zep1994 <thomasmatlockbba@gmail.com>
2023-07-18 07:08:09 -0500

    Connect to API

---
+++ Flush_Client/Models/Ingredient.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flush_Client.Models
+{
+    class Ingredient : INotifyPropertyChanged
+    {
+        int _id;
+        public int Id {
+            get => _id;
+            set
+            {
+                if (_id == value) return;
+                _id = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Id)));
+            }
+        }
+        string _name;
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (_name == value) return;
+                _name = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Name)));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+    }
+}
